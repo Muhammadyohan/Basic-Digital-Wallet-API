06df96437c1bb879f29ea56c7b9434addfa7983e
Muhammadyohan Kachey <6410110421@email.psu.ac.th>
2024-08-03 15:13:52 +0700

    add http get methods for get items or an item from db

--- digital_wallet/main.py
+++ digital_wallet/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
-from sqlmodel import create_engine, SQLModel, Session, select # type: ignore
+from sqlmodel import create_engine, SQLModel, Session, select  # type: ignore
 
 from models.item import Item, CreateItem, UpdateItem, ItemList, DBItem
 
@@ -30,13 +30,34 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/items")
+@app.post("/item")
 async def create_item(item: CreateItem) -> Item:
     data = item.dict()
-    db_item = DBItem(**data)
+    dbItem = DBItem(**data)
     with Session(engine) as db:
-        db.add(db_item)
+        db.add(dbItem)
         db.commit()
-        db.refresh(db_item)
+        db.refresh(dbItem)
 
-    return Item.from_orm(db_item)
+    return Item.from_orm(dbItem)
+
+
+@app.get("/items")
+async def get_items(page: int = 1, page_size: int = 10) -> ItemList:
+    with Session(engine) as db:
+        dbItems = db.exec(
+            select(DBItem).offset((page - 1) * page_size).limit(page_size)
+        ).all()
+
+    return ItemList(
+        items=dbItems, page=page, page_size=page_size, size_per_page=len(dbItems)
+    )
+
+
+@app.get("/item/{item_id}")
+async def get_item(item_id: int) -> Item:
+    with Session(engine) as db:
+        dbItem = db.get(DBItem, item_id)
+        if dbItem is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return Item.from_orm(dbItem)
