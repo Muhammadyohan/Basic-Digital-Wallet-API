6249a346af1a390694737b2fc4528a90be3e3bc2
Muhammadyohan Kachey <6410110421@email.psu.ac.th>
2024-08-03 19:50:26 +0700

    add get methods for merchant

--- digital_wallet/routers/merchants.py
+++ digital_wallet/routers/merchants.py
@@ -20,4 +20,28 @@ async def create_merchant(item: CreateMerchant) -> Merchant:
         db.refresh(db_item)
 
     return Merchant.from_orm(db_item)
-    
+
+
+@router.get("/merchants", tags=["merchant"])
+async def get_merchants(page: int = 1, page_size: int = 10) -> MerchantList:
+    with Session(engine) as db:
+        db_merchants = db.exec(
+            select(DBMerchant).offset((page - 1) * page_size).limit(page_size)
+        ).all()
+
+    return MerchantList(
+        merchants=db_merchants,
+        page=page,
+        page_size=page_size,
+        size_per_page=len(db_merchants),
+    )
+
+
+@router.get("/merchant/{merchant_id}", tags=["merchant"])
+async def get_merchant(merchant_id: int) -> Merchant:
+    with Session(engine) as db:
+        db_merchant = db.get(DBMerchant, merchant_id)
+        if db_merchant is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+    return Merchant.from_orm(db_merchant)
